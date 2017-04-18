df253d9bc26c95d28b87e6360b9f0908bafc69dd
Tanisha Shrotriya <tshrotriya@localhost.localdomain>
2017-04-18 17:15:33 +0530

    April 18th 17

--- 1f_add_display.asm
+++ 1f_add_display.asm
@@ -1,45 +1,109 @@
 ;Positive negative counter
 
+%macro display 2
+
+mov rax,1
+mov rdi,1
+mov rsi,%1
+mov rdx,%2
+syscall
+
+%endmacro
 
 
-section .data
+%macro input 2
+
+mov rax,1
+mov rdi,1
+mov rsi,%1
+mov rdx,%2
+syscall
+
+%endmacro
+
+section .data 
  
-arr dw 12H,0FFH,00,-21,54H,-10,000,265,076H,07654H
-var db 0                        ;zero cnt
+	arr   dw 12H,0FFH,00,-21,54H,-10,000,265,076H,07654H
+	zero  db 0H              ;zero cnt
+	pos_  db 0H              ;positive counter     
+        neg_  db 0H              ;negative counter
+        len   db 0H              ;for length of res
 
+section .bss 
+
+        res :resb 3
+         
+ 
 section .text
 
 global _start
 _start:
 
-mov bl,0			;+ve counter
-mov cl,0			;negative counter
 mov dl,0			;array counter
 mov esi,arr
 
 again:
-mov al,[esi]
-add al,0
-js neg
-jz zero
-inc bl
-jmp next
+	mov al,[esi]
+	add al,0
+	js neg
+	jz zero_
+	inc byte[pos_]
+	jmp next
 
 neg:
-inc cl
-jmp next
+	inc byte[neg_]
+	jmp next
 
-zero:
-mov al,[var]
-add al,1
-mov [var],al
+zero_:
+	inc byte[zero]
 next:
-inc dl
-add esi,2
-cmp dl,10
-jl again
+	inc dl
+	add esi,2              ; because arr is in dw size
+	cmp dl,10
+	jl again
 
-mov al,[var]
+;mov al,[neg_]
+;call pack
+;mov al,[pos_]
+;call pack
+mov al,[zero]
+call pack
+
+
 mov rax,60
 mov rdi,0
 syscall
+
+pack:
+
+        mov dx,0
+   	mov cl,0
+        mov bl,10 
+        mov edi,res
+	push_:
+  		div bl
+  	        push dx
+   		inc cl
+   		cmp al,0
+                jnz push_
+   
+	pop_:
+     		pop dx
+   		add dx,30H
+   		mov [edi],dx
+   		inc edi 
+   		dec cl
+   		cmp cl,0
+   		jnz pop_
+
+;		mov esi,[res]
+;	countBytes:
+;		inc cl
+ ;               inc esi
+;		cmp esi,edi
+;		jnz countBytes
+        
+        mov byte[len],1
+	display res,[len]
+
+ret
